508f10280bc1c9fc50ead14cdfdb5ef1bb2ea6f0
Aditya Singh <adityasingh@berkeley.edu>
2022-04-04 01:23:10 -0700

    add button and pr features

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   def self.populate_from_airtable
-    @@client = Airtable::Client.new(ENV['airtable_key'])
-    @@table = @@client.table(ENV['airtable_app_key'], ENV['airtable_table_id'])
+    ENV["airtable_key"] = "keyreuExTdz41S45K" #temporary, will be replaced by figaro + config/application.yml
+    @@client = Airtable::Client.new(ENV["airtable_key"])
+    @@table = @@client.table("appBVUbGPToiaKXQK", "tblUm0begj9P1Jz8J")
     @@records = @@table.records
-    User.delete_all # could be replaced by if checks to prevent duplicate Users
+    User.delete_all #could be replaced by if checks to prevent duplicate Users
     @@records.each do |record|
-      User.create({ name: record[:name], email: record[:email] })
+      User.create({name: record[:name], email: record[:email]})
     end
   end
 
