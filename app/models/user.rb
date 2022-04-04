22d7b5f54b3928c7880b5ede791835e68cd537ed
Aditya Singh <adityasingh@berkeley.edu>
2022-04-04 01:23:10 -0700

    populate users from airtable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  def self.populate_from_airtable
+    ENV["airtable_key"] = "keyreuExTdz41S45K" #temporary, will be replaced by figaro + config/application.yml
+    @@client = Airtable::Client.new(ENV["airtable_key"])
+    @@table = @@client.table("appBVUbGPToiaKXQK", "tblUm0begj9P1Jz8J")
+    @@records = @@table.records
+    User.delete_all #could be replaced by if checks to prevent duplicate Users
+    @@records.each do |record|
+      User.create({name: record[:name], email: record[:email]})
+    end
+  end
+
 end
