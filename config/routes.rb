e243e1a14e9a42396d9fe7f77c85353e67b17ca6
Simon Jovanovic <simonjov@berkeley.edu>
2022-04-04 16:49:15 -0700

    refresh button attempt 9

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'sessions#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
-  get 'update', to: 'sessions#airtable_update', as: 'update'
+  post 'update' => 'sessions#airtable_update'
   delete 'logout' => 'sessions#destroy'
   resources :announcements
   resources :tasks
