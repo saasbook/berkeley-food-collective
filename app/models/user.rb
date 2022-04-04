be3166bcdfeb84bf36412a2ec2014da0de339070
Aditya Singh <adityasingh@berkeley.edu>
2022-04-04 02:29:30 -0700

    Added figaro/encryption and pushed Gemfile with airtable and figaro

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   def self.populate_from_airtable
-    ENV["airtable_key"] = "keyreuExTdz41S45K" #temporary, will be replaced by figaro + config/application.yml
     @@client = Airtable::Client.new(ENV["airtable_key"])
-    @@table = @@client.table("appBVUbGPToiaKXQK", "tblUm0begj9P1Jz8J")
+    @@table = @@client.table(ENV["airtable_app_key"], ENV["airtable_table_id"])
     @@records = @@table.records
     User.delete_all #could be replaced by if checks to prevent duplicate Users
     @@records.each do |record|
