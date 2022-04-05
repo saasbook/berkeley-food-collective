27b7964ea76e8bb83d460b5e707ffe00f8055769
Simon Jovanovic <simonjov@berkeley.edu>
2022-04-05 05:47:33 -0700

    .each task attempt 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,7 @@ class TasksController < ApplicationController
     task.completed = !task.completed
     user = User.find(params[:user])
     task.user_complete = user.name
-    task.complete_time = DateTime.now.change(:offset => '+0700')
+    task.complete_time = DateTime.now.in_time_zone(ActiveSupport::TimeZone.new("Pacific Time (US & Canada)"))
     task.save!
     redirect_to tasks_path
   end
