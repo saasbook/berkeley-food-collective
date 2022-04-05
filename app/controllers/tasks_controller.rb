b2e7dd5e7b75bcb7a6d92546e18cccb4e5ca17ba
Simon Jovanovic <simonjov@berkeley.edu>
2022-04-05 05:38:22 -0700

    .each task attempt 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,7 @@ class TasksController < ApplicationController
     task.completed = !task.completed
     user = User.find(params[:user])
     task.user_complete = user.name
-    task.complete_time = DateTime.now
+    task.complete_time = DateTime.current
     task.save!
     redirect_to tasks_path
   end
