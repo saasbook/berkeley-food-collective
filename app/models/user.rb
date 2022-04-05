7c2961248246b1f763615f0cf92de56e10edbf56
Simon Jovanovic <simonjov@berkeley.edu>
2022-04-04 17:23:23 -0700

    refresh button attempt 12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   def self.populate_from_airtable
-    @client = Airtable::Client.new(ENV['airtable_key'])
-    @table = @client.table(ENV['airtable_app_key'], ENV['airtable_table_id'])
-    @records = @table.records
+    @@client = Airtable::Client.new(ENV['airtable_key'])
+    @@table = @@client.table(ENV['airtable_app_key'], ENV['airtable_table_id'])
+    @@records = @@table.records
     User.delete_all # could be replaced by if checks to prevent duplicate Users
-    @records.each do |record|
+    @@records.each do |record|
       User.create({ name: record[:name], email: record[:email] })
     end
 
