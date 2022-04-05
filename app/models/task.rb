461e9fcb7b85b0db6c047ee2e9b270a082a21579
Simon Jovanovic <simonjov@berkeley.edu>
2022-04-05 04:29:08 -0700

    .each task attempt 1

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,13 @@
 class Task < ApplicationRecord
+
+  attr_accessor :completed
+
   def self.clear_tasks
     Task.delete_all
   end
 
   def self.checkmarker
     @task = Task.first
-    @task.completed = false
+    @task.completed = !@task.completed
   end
 end
