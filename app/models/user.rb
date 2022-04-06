6d98d3ee1a1c6f47463a24e71f32edcc996e8a44
andrewmopro <70362887+andrewmopro@users.noreply.github.com>
2022-04-06 12:57:48 -0700

    Revert "181708826 airtable user populate"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,2 @@
 class User < ApplicationRecord
-  def self.populate_from_airtable
-    @@client = Airtable::Client.new(ENV["airtable_key"])
-    @@table = @@client.table(ENV["airtable_app_key"], ENV["airtable_table_id"])
-    @@records = @@table.records
-    User.delete_all #could be replaced by if checks to prevent duplicate Users
-    @@records.each do |record|
-      User.create({name: record[:name], email: record[:email]})
-    end
-  end
-
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,11 +4,4 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-
-  test "should get airtable record" do
-    User.populate_from_airtable
-    assert User.find_by(email: "adi@example.com") != nil
-    #assert User.find_by(email: "andrewmo@berkeley.edu") == nil #depends on whether db is wiped or not on populate call
-  end
-
 end
