bbf80853fad11bdb7448dde4cb9a3f7819dc75af
Simon Jovanovic <simonjov@berkeley.edu>
2022-04-07 04:45:17 -0700

    routing logout properly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    logout
+    redirect_to login_path
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,10 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def logout
+    session[:user_id] = nil
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
