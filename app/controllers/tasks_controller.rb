79cc39d23ecb7b52499b56c93e2361b7b1344af3
Simon Jovanovic <simonjov@berkeley.edu>
2022-04-07 10:03:01 -0700

    implement priority display

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,11 +26,34 @@ class TasksController < ApplicationController
     new_task.name = params[:task_name]
     new_task.description = params[:task_description]
     new_task.category = params[:task_category]
-    # new_task.priority = params[:task_priority] add task priority somehow
+    new_task.priority = params[:priority]
     new_task.added = DateTime.now
     new_task.user_add = current_user.name
     new_task.completed = false
     new_task.save!
     redirect_to tasks_path
+  end
+
+  def convert_priority(priority)
+    if priority.nil?
+      return 0
+    end
+    ret_str = ''
+    (1..priority).each do |_|
+      ret_str += '!'
+    end
+    ret_str
+  end
+
+  def priority_color(priority)
+    if priority.nil?
+      'text-primary'
+    elsif priority == 1
+      'text-success'
+    elsif priority == 2
+      'text-warning'
+    else
+      'text-danger'
+    end
   end
 end
