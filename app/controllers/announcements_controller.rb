cf6980013e6a4ccafd59f306b6c06cb0592691f9
jtb <jadenbryan@berkeley.edu>
2022-04-12 18:03:15 -0700

    progress

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,5 +1,6 @@
 class AnnouncementsController < ApplicationController
   def index
+    @announcements = Announcement.populate_from_airtable
   end
 
   def create
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,7 +1,13 @@
+require 'active_support/all'
+
 class Announcement < ApplicationRecord
     def self.populate_from_airtable
-        @client = Airtable::Client.new("keyuszZsBlgcxdMAy")
-        @table = @client.table("keyuszZsBlgcxdMAy", "Test")
-        @records = @table.records
-        byebug 
+        @@client = Airtable::Client.new("keyuszZsBlgcxdMAy")
+        @@table = @client.table("keyuszZsBlgcxdMAy", "announcements")
+        @@records = @table.records
+        Announcement.delete_all
+        @@records.each do |record|
+            Announcement.create({message: record[:event_name], eventtime:[:event_time]})
+        end 
+    end
 end
