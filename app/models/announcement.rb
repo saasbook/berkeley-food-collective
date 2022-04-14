c8212dde04fc0744804e2d3b05ec86b1ff85591c
jtb <jadenbryan@berkeley.edu>
2022-04-14 03:06:44 -0700

    testing if changing key name makes a difference in build

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,7 +2,7 @@ require 'active_support/all'
 
 class Announcement < ApplicationRecord
     def self.populate_from_airtable
-        @client = Airtable::Client.new(ENV["airtable_api_key"])
+        @client = Airtable::Client.new(ENV["airtable_key"])
         @table = @client.table(ENV["airtable_app_key"], "tblI0sVfVCRN7Wwla")
         @records = @table.records
         Announcement.delete_all
