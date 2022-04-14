cfad749c3ce88fbac532551587bb05bbf12d29a5
jtb <jadenbryan@berkeley.edu>
2022-04-14 02:58:16 -0700

    ladies and gentlemen, we can see airtable announcements

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,8 +2,8 @@ require 'active_support/all'
 
 class Announcement < ApplicationRecord
     def self.populate_from_airtable
-        @client = Airtable::Client.new("keyuszZsBlgcxdMAy")
-        @table = @client.table("appxMs7cCMrVilgD0", "tblAXuNilYQjY1gex")
+        @client = Airtable::Client.new(ENV["airtable_api_key"])
+        @table = @client.table(ENV["airtable_app_key"], "tblI0sVfVCRN7Wwla")
         @records = @table.records
         Announcement.delete_all
         @records.each do |record|
