2ac7ae95b6bc895ed0241c2f2be9036b9f980db5
jtb <jadenbryan@berkeley.edu>
2022-04-14 15:05:51 -0700

    fix borders

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -2,6 +2,7 @@ class AnnouncementsController < ApplicationController
   def index
     Announcement.populate_from_airtable
     @announcements = Announcement.order("eventtime DESC")
+    @count = 0
   end
 
   def create
