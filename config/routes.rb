d45a9bd0728e4e36cfc0c75fc4c39af97862f6c5
simonjov <73329994+simonjov@users.noreply.github.com>
2022-04-16 16:24:59 -0700

    get logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   post 'checkmark' => 'tasks#checkmark'
-  post 'logout' => 'sessions#destroy'
+  get 'logout' => 'sessions#destroy'
 
   resources :announcements
   resources :tasks
