9249c730942fe28f320ba14a31d19bbb05b99d30
Aditya Singh <adityasingh@berkeley.edu>
2022-04-17 23:05:16 -0700

    email to E-mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  def self.populate_from_airtable
+    @@client = Airtable::Client.new("keyreuExTdz41S45K")
+    @@table = @@client.table("appBVUbGPToiaKXQK", "tblUm0begj9P1Jz8J")
+    @@records = @@table.records
+    User.delete_all #could be replaced by if checks to prevent duplicate Users
+    @@records.each do |record|
+      User.create({name: record["Full Name"], email: record["E-mail"]})
+    end
+  end
 end
