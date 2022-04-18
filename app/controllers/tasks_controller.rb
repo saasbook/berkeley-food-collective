5ed2017e7bf356a64a0d0c69157dad02eef690a4
simonjov <73329994+simonjov@users.noreply.github.com>
2022-04-17 23:38:54 -0700

    move css, clean up tasks#index, use time.current

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,10 +8,10 @@ class TasksController < ApplicationController
              else
                Task.where(category: @filter)
              end
-    incomplete_task = @tasks.where(completed: false).sort_by { |task| [-task.priority, task.added] }
-    completed_tasks = @tasks.where(completed: true).sort_by { |task| [task.complete_time] }.reverse
+    incomplete_task = @tasks.where(completed: false).order(priority: :desc, added: :asc)
+    completed_tasks = @tasks.where(completed: true).order(:complete_time)
     @tasks = incomplete_task + completed_tasks
-    @num_incomplete = @tasks.select { |task| task.completed == false }.count
+    @num_incomplete = incomplete_task.count
     @count = 0
     @completed = false
     @curr_categories = @all_categories
@@ -43,7 +43,7 @@ class TasksController < ApplicationController
     new_task.description = params[:task_description]
     new_task.category = params[:task_category]
     new_task.priority = params[:priority]
-    new_task.added = DateTime.now
+    new_task.added = Time.current
     new_task.user_add = current_user.name
     new_task.completed = false
     new_task.save!
@@ -60,7 +60,7 @@ class TasksController < ApplicationController
     task.description = params[:task_description]
     task.category = params[:task_category]
     task.priority = params[:priority]
-    task.added = DateTime.now
+    task.added = Time.current
     task.user_add = current_user.name
     task.save!
     if params[:task_category] == params[:category]
