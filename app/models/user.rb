19059f41c8f903124684a6fe2ccebf225baeb32d
Aditya Singh <adityasingh@berkeley.edu>
2022-04-19 10:44:42 -0700

    basic populate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   def self.populate_from_airtable
-    @@client = Airtable::Client.new("keyreuExTdz41S45K")
-    @@table = @@client.table("appBVUbGPToiaKXQK", "tblUm0begj9P1Jz8J")
-    @@records = @@table.records
+    @@client = Airtable::Client.new(ENV['airtable_api_key'])
+    @@table = @@client.table(ENV['airtable_app_key'], "tbl1SuoIW99KUjDMx")
+    @@records = @@table.all
     User.delete_all #could be replaced by if checks to prevent duplicate Users
     @@records.each do |record|
-      User.create({name: record["Full Name"], email: record["E-mail"]})
+      User.create({name: record["Full Name"], email: record["E-mail:"]})
     end
   end
 end
