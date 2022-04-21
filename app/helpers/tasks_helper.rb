1bcfe910e20fdd95c745137f87594703980f1331
simonjov <73329994+simonjov@users.noreply.github.com>
2022-04-21 12:39:07 -0700

    airtable logo for medium

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -12,7 +12,7 @@ module TasksHelper
 
   def priority_visualizer(priority)
     if priority == 2
-      image_tag 'airtable_logo.png', { alt: 'Airtable Logo', width: 20, height: 20 }
+      image_tag 'airtable_logo.png', { alt: 'Airtable Logo' }
     else
       '!' * priority
     end
