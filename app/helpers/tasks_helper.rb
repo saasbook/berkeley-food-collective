4f550d5db2fd57760ed6e2573c068fabd68c9f44
simonjov <73329994+simonjov@users.noreply.github.com>
2022-04-21 12:36:29 -0700

    airtable logo for medium

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -3,16 +3,18 @@ module TasksHelper
     case priority
     when 1
       'text-success'
-    when 2
-      'text-warning'
     when 3
       'text-danger'
     else
-      'text-primary'
+      ''
     end
   end
 
   def priority_visualizer(priority)
-    '!' * priority
+    if priority == 2
+      image_tag 'airtable_logo.png', { alt: 'Airtable Logo', width: 25, height: 25 }
+    else
+      '!' * priority
+    end
   end
 end
