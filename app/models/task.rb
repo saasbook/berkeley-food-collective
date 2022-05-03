38c5f49f6189788d1ce785a479f90e5dad6adf43
simonjov <73329994+simonjov@users.noreply.github.com>
2022-05-02 18:28:32 -0700

    tasks update stuff

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,22 +1,24 @@
 class Task < ApplicationRecord
 
   def self.populate_from_airtable(user_email)
-    @client = Airtable::Client.new(ENV["airtable_api_key"])
-    @table = @client.table(ENV["airtable_app_key"], "tblwRWq2x609uUzOv")
+    @client = Airtable::Client.new(ENV['airtable_api_key'])
+    @table = @client.table(ENV['airtable_app_key'], 'tblwRWq2x609uUzOv')
     @records = @table.records
+    Task.where(priority: 2).delete_all unless Task.last.added.today?
     @records.each do |record|
-      @shift_assignment_array = record["e_mail:_(from_members)_(from_shift_assignment)"]
+      @shift_assignment_array = record['e_mail:_(from_members)_(from_shift_assignment)']
       @tmpassigneduserstring = ''
-      if !@shift_assignment_array.empty?
+      unless @shift_assignment_array.empty?
         @shift_assignment_array.each do |user|
-          @tmpassigneduserstring = @tmpassigneduserstring + user
+          @tmpassigneduserstring += user
         end
       end
       if @shift_assignment_array.length != 0 && @shift_assignment_array.include?(user_email) 
         markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
-        @parsed_description = markdown.render(record["Description:"])
-        if !Task.exists?(name: record[:name])
-          Task.create({name: record[:name], description: @parsed_description, category: "Airtable", priority: 2, user_add: "BSFC", completed: false, assigneduserstring: @tmpassigneduserstring})
+        @parsed_description = markdown.render(record['Description:'])
+        unless Task.exists?(name: record[:name])
+          Task.create({ name: record[:name], description: @parsed_description, category: 'Airtable',
+                        priority: 2, user_add: 'BSFC', completed: false, assigneduserstring: @tmpassigneduserstring })
         end
       end
     end
