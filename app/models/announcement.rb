1fa06a10a65afd134ad1cc15f72c41a0b336a877
Aditya Singh <adityasingh@berkeley.edu>
2022-05-03 00:08:21 -0700

    remove .byebug_history and remove @ from annoucenment.rb

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -4,17 +4,17 @@ require 'active_support/all'
 
 class Announcement < ApplicationRecord
   def self.populate_from_airtable
-    @client = Airtable::Client.new(ENV['airtable_api_key'])
-    @table = @client.table(Setting.last.airtable_base_id, Setting.last.announcements_table_id)
-    @records = @table.records
+    client = Airtable::Client.new(ENV['airtable_api_key'])
+    table = client.table(Setting.last.airtable_base_id, Setting.last.announcements_table_id)
+    records = table.records
     Announcement.delete_all
-    @records.each do |record|
-      @recorded_author = if record[:submitteremail].length.zero?
+    records.each do |record|
+      recorded_author = if record[:submitteremail].length.zero?
                            'BSFC'
                          else
                            record[:submitteremail][0]
                          end
-      Announcement.create({ message: record[:event_name], eventtime: record[:event_time],audience: @recorded_author,
+      Announcement.create({ message: record[:event_name], eventtime: record[:event_time],audience: recorded_author,
                             location: record[:event_location] })
     end
   end
