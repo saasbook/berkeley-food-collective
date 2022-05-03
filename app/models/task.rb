36560aa1b87c858866aa4ebf4d0efb00987c4da9
jtb <jadenbryan@berkeley.edu>
2022-05-02 21:20:10 -0700

    .all

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
   def self.populate_from_airtable(user_email)
     @client = Airtable::Client.new(ENV['airtable_api_key'])
     @table = @client.table(ENV['airtable_app_key'], 'tblwRWq2x609uUzOv')
-    @records = @table.records
+    @records = @table.records.all
     #Task.where(priority: 2).delete_all unless Task.last.added.today?
     @records.each do |record|
       shift_assignment_array = record['e_mail:_(from_members)_(from_shift_assignment)']
